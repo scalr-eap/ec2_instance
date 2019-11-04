331b949105ce620c555f5b007313ec917fe6ae9f
Ryan <ryan.fee625@gmail.com>
2019-11-04 14:26:28 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  access_key = "${var.scalr_aws_access_key}"
+  secret_key = "${var.scalr_aws_secret_key}"
+  region     = var.region
+}
+
+resource "aws_instance" "scalr" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet
+  vpc_security_group_ids = var.sg
+  key_name               = var.key
+}
