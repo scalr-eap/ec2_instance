ccaf815c9cc133baaeb39ba82f96936b7d2b92b5
Ryan <ryan.fee625@gmail.com>
2019-11-06 10:28:39 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 }
 
 variable "subnet" {
-  description = "Subnet ID"
+description = "Subnet ID"
 }
 
 variable "sg" {
@@ -28,5 +28,5 @@ variable "key" {
 }
 
 variable "vpc_id" {
-  description = "VPC"
+description = "VPC"
 }
