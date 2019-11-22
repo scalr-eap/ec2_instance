b3c7273414fdbe017f3c3ef8802968ec5f667ddc
Ryan <ryan.fee625@gmail.com>
2019-11-22 14:48:45 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  access_key = "${var.scalr_aws_access_key}"
-  secret_key = "${var.scalr_aws_secret_key}"
+  access_key = var.scalr_aws_access_key
+  secret_key = var.scalr_aws_secret_key
   region     = var.region
 }
 
 data "null_data_source" "values" {
   inputs = {
-    vpc_id = "${var.vpc_id}"
+    vpc_id = var.vpc_id
     }
 }
 
