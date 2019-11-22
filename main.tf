c8f3ba48325dabe2f9dbca39af358ba6c6e1d8fc
Ryan <ryan.fee625@gmail.com>
2019-11-22 15:17:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = var.scalr_aws_access_key
-  secret_key = var.scalr_aws_secret_key
+  access_key = "${var.scalr_aws_access_key}"
+  secret_key = "${var.scalr_aws_secret_key}"
   region     = var.region
 }
 
